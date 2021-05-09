d8f5e67a9a8a02bbebcde5fcc862805c57f65245
Cody Huang <wenhuang@umail.ucsb.edu>
2021-05-09 15:51:20 -0700

    AJAX Form Submit added

--- application/public/js/scratch.js
+++ application/public/js/scratch.js
@@ -1,6 +1,6 @@
 const name = document.getElementById('username')
 const email = document.getElementById('email')
-email.setAttribute("type","email")
+email.setAttribute('type','email')
 const password = document.getElementById('password')
 const form = document.getElementById('form')
 const passConfirm = document.getElementById('passConfirm')
--- application/routes/posts.js
+++ application/routes/posts.js
@@ -1,15 +1,68 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../conf/database');
-const { successPrint, errorPrint, requestPrint } = require('../helpers/debug/debugprinters');
+const { successPrint, errorPrint} = require('../helpers/debug/debugprinters');
 var sharp = require('sharp');
 var multer = require('multer');
 var crypto = require('crypto');//to mangle file names
-var PostError = requestPrint('../helpers/error/PostError');
+var PostError = require('../helpers/error/PostError');
+const { response } = require('../app');
 
-router.post('/createPost', (req, res, next) => {
-    console.log(req);
-    res.send('');
-})
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, "public/images/uploads");
+    },
+    filename: function(req, file, cb){
+        let fileExt = file.mimetype.split('/')[1];
+        let randomName = crypto.randomBytes(22).toString("hex");
+        cb(null, `${randomName}.${fileExt}`);
+    }
+});
+
+var uploader = multer({storage: storage});
+
+router.post('/createPost', uploader.single("uploadImage"),(req, res, next) => {
+    let fileUploaded = req.file.path;
+    let fileAsThumbnail = `thumbnail-${req.file.filename}`;
+    let destinationOfThumbnail = req.file.destination + "/" + fileAsThumbnail;
+    let title = req.body.title;
+    let description = req.body.description;
+    let fk_userId = req.session.userId;
+
+    /**
+     * do server validation, make sure fk_userId and title is not empty
+     * if any values that used for insert statement
+     * are undefined, mysql.query or exeute will fail
+     * with following error:
+     * Bind parameters cannot be underfined
+     */
+    sharp(fileUploaded)
+    .resize(200)
+    .toFile(destinationOfThumbnail)
+    .then(() => {
+        let baseSQL = 'INSERT INTO posts (title, description, photopath, thumbnail, created, fk_userid) VALUES (?,?,?,?,now(),?);';
+        return db.execute(baseSQL,[title, description, fileUploaded, destinationOfThumbnail,fk_userId]);  
+    })
+    .then(([results, fields]) => {
+        if(results && results.affectedRows){
+            req.flash('success', "Post created succesfully");
+            res.json({status:"OK", message:"post was created","redirect":"/"});
+        }else{
+            res.json({status:"fail", message:"post was not created","redirect":"/postimage"});
+            //throw new PostError('Post could not be created', 'postImage', 200);
+        }
+    })
+    .catch((err) => {
+        if(err instanceof PostError){
+            errorPrint(err.getMessage());
+            req.flash('error', err.getMessage());
+            res.status(err.getStatus());
+            res.redirect(err.getRedirectURL());
+        }else{
+            next(err);
+        }
+    })
+});
 
 module.exports = router;
