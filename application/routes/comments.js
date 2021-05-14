481a4798bc88b8dce28d866f462dc3d96ee64e49
Cody Huang <wenhuang@umail.ucsb.edu>
2021-05-14 16:01:11 -0700

    added route to create comment

--- application/routes/comments.js
+++ application/routes/comments.js
@@ -3,9 +3,41 @@ var router = express.Router();
 const { successPrint, errorPrint } = require("../helpers/debug/debugprinters");
 const { create } = require('../models/Comments');
 
-router.post('/create', (req,res,next) => {
-    console.log(req.body);
-    res.json("comment received!");
+router.post('/create', (req, res, next) => {
+    if (!req.session.username) {
+        errorPrint("Must be logged in to comment");
+        res.json({
+            code: -1,
+            status: "danger",
+            message: "Must be logged in to comment"
+        });
+    } else {
+        let { comment, postId } = req.body;
+        let username = req.session.username;
+        let userId = req.session.userId;
+
+        create(userId, postId, comment)
+            .then((wasSuccessful) => {
+                if (wasSuccessful != -1) {
+                    successPrint(`comment was created for ${username}`);
+                    res.json({
+                        code: 1,
+                        status: "sucess",
+                        message: "comment created",
+                        comment: comment,
+                        username: username
+                    })
+                } else {
+                    errorPrint('comment was not saved');
+                    res.json({
+                        code: -1,
+                        status: "danger",
+                        message: "comment was not created"
+                    })
+                }
+            })
+            .catch((err) => next(err));
+    }
 })
 
 module.exports = router;
